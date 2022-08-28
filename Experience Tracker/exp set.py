0ad91dd72d63591136d6122947ccffdad00171e4
James <32420525+Jtrinh3@users.noreply.github.com>
2022-08-28 15:07:38 -0500

    Fix deprecated command.

--- Experience Tracker/exp set.py
+++ Experience Tracker/exp set.py
@@ -4,9 +4,9 @@ embed
 experience = int(get("exp"))
 arg1 = '&1&' if '&1'+'&' != '&1&' else ''
 experience = int(float(arg1))
-set_cvar_nx("exp", experience)
+character().set_cvar_nx("exp", experience)
 </drac2>
 -title "{{f"{name} experience is set to __{exp}__."}}"
 -desc "That's a lot of experience!"
 -thumb "https://cdn.discordapp.com/attachments/683614763716050944/777256220876341248/book-pile.png"
--footer "!help exp | made by James#8096"
+-footer "!help exp | made by Jaymes#8096"
