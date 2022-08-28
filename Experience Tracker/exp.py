ce09a6915db93cfe5ebd05b5a3d9d5e6b8061023
James <32420525+Jtrinh3@users.noreply.github.com>
2022-08-28 15:07:58 -0500

    Update exp.py

--- Experience Tracker/exp.py
+++ Experience Tracker/exp.py
@@ -1,7 +1,7 @@
 embed
-{{set_cvar_nx("exp",0)}}
+{{character().set_cvar_nx("exp",0)}}
 -title "{{f"{name} currently have __{exp}__ experience"}}"
 -desc "Keep up the good work!
 Add or set exp with `!exp <add/set> <#>`"
 -thumb "https://cdn.discordapp.com/attachments/683614763716050944/777256220876341248/book-pile.png"
--footer "!help exp | made by James#8096"
+-footer "!help exp | made by Jaymes#8096"
